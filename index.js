c7aa1025b28c318600ec0a1a0c245f999a696039
JSirial429 <jos0928@gmail.com>
2021-03-09 02:23:53 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,111 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+  constructor() {
 
-  get(pos) {}
+    this.items = [];
+    this.length = this.items.length;
 
-  max() {}
+  }
 
-  min() {}
+  add(item) {
 
-  sum() {}
+    this.items.push(item);
+    this.items.sort( (a,b) =>{
+      return a-b;
+    });
 
-  avg() {}
+    this.length = this.items.length;
+
+  }
+
+  get(pos) {
+
+    if(pos > this.length){
+
+      throw new Error('OutOfBounds');
+
+    }
+    else{
+
+      return this.items[pos];
+
+    }
+
+  }
+
+  max() {
+    
+    let maxVal = 0;
+
+    if(this.length === 0){
+      
+      throw new Error('EmptySortedList');
+
+    }
+    else{
+
+      this.items.forEach( (item) => {
+
+        if(item>maxVal){
+
+          maxVal = item;
+
+        }
+
+      });
+
+      return maxVal;
+
+    }
+  
+  }
+
+  min() {
+
+    if(this.length === 0){
+      
+      throw new Error('EmptySortedList');
+
+    }
+    else{
+
+      let minVal = this.items[0];
+
+      this.items.forEach( (item) => {
+
+        if(item<minVal){
+
+          minVal = item;
+
+        }
+
+      });
+
+      return minVal;
+
+    }
+
+  }
+
+  sum() {
+
+    let sum = 0;
+
+    this.items.forEach( (item) => {
+
+      this.sum+=item;
+
+    });
+
+    return sum;
+
+  }
+
+  avg() {
+
+
+
+  }
 }
 
 module.exports = SortedList;
